d4257da99616fde6832e0d740d8e54430f35f8a2
Manuel Heredia <manolo.heredia@gmail.com>
2021-08-04 23:49:38 +0200

    Solution to object-detection and visual_lane_servoing exercises

--- object-detection/solution/utils/data_collection.py
+++ object-detection/solution/utils/data_collection.py
@@ -21,55 +21,75 @@ class SkipException(Exception):
     pass
 
 # Need to change this dataset directory if not running inside docker container... TODO fix
+# MH: Outside container, this corresponds to ~/mooc-exercises/object-detection/solution/duckietown_dataset/
 DATASET_DIR="/jupyter_ws/solution/duckietown_dataset"
 IMAGE_SIZE=416
-SPLIT_PERCENTAGE=0.8
+SPLIT_PERCENTAGE=0.8 # MH: % of images saved in train vs val folders
 
 
-npz_index = 0
+npz_index = 0 # counter for image names
 
-def save_npz(img, boxes, classes):
+def save_npz(img, seg, boxes, classes):
     global npz_index
 
+    # MH: this is the function that saves an image and writes boxes in label
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     cv2.imwrite(f"{DATASET_DIR}/images/{npz_index}.jpg", img)
+    # MH: uncomment to save also segmented image
+    # cv2.imwrite(f"{DATASET_DIR}/images/{npz_index}_seg.jpg", seg)
+
     boxes = np.array([xminyminxmaxymax2xywfnormalized(box, IMAGE_SIZE) for box in boxes])
     with open(f"{DATASET_DIR}/labels/{npz_index}.txt", "w") as f:
         for i in range(len(boxes)):
             f.write(f"{classes[i]} "+" ".join(map(str,boxes[i]))+"\n")
 
+    print("MH: Saved image: ", npz_index,".jpg")
     npz_index += 1
 
+    
+    
 # some setup
 seed(123)
-MAX_STEPS = 10
+MAX_STEPS = 1000 # MH: number of images saved. Changed to 200
 nb_of_steps = 0
 
-# we interate over several maps to get more diverse data
+# we iterate over several maps to get more diverse data
 possible_maps = [
     "loop_pedestrians",
     "udem1",
     "loop_dyn_duckiebots",
     "zigzag_dists"
 ]
-env_id = 0
+env_id = 0 # MH: initialize with the first type of map
 env = None
+
 while True:
     if env is not None:
-        env.window.close()
+        # MH: comment out next line if line 99 is commented out or it will sop early
+        # (following advice from https://github.com/duckietown/mooc-exercises/issues/10)
+        # env.window.close()
+        
         env.close()
 
     if env_id >= len(possible_maps):
-        env_id = env_id % len(possible_maps)
+        env_id = env_id % len(possible_maps) # MH: cycle with env_id in the possible_maps
     env = launch_env(possible_maps[env_id])
+    print("MH: Using map: ", possible_maps[env_id], "...") #MH: for debugging
+    
     policy = PurePursuitPolicy(env)
     obs = env.reset()
-
-    inner_steps = 0
+    
+    #MH:looks like this starts a while loop for collecting images in a given map
+    
+    inner_steps = 0 # MH: 
+    
+    # MH get out after MAX_STEPS 
     if nb_of_steps >= MAX_STEPS:
-        break
+        break 
 
     while True:
+        # MH: get out after MAX_STEPS of 100 inner steps
+        
         if nb_of_steps >= MAX_STEPS or inner_steps > 100:
             break
 
@@ -79,28 +99,37 @@ while True:
         seg = env.render_obs(True)
 
 
-        obs = cv2.resize(obs, (IMAGE_SIZE, IMAGE_SIZE))
-        seg = cv2.resize(seg, (IMAGE_SIZE, IMAGE_SIZE))
+        obs = cv2.resize(obs, (IMAGE_SIZE, IMAGE_SIZE)) # MH this is the snapshot
+        seg = cv2.resize(seg, (IMAGE_SIZE, IMAGE_SIZE)) # MH this is the segmented image
 
-        #env.render(segment=True)
+        # MH: uncomment next line to see segmented images in  a window, but then comment out also line 63 
+        # env.render(segment=True)
 
         try:
+            # MH extract boxes from segmented image
             boxes, classes = find_all_boxes_and_classes(seg)
         except SkipException as e:
             print(e)
             continue
 
+        # MH: comment out to prevent from saving the images with the boxes
+        """
         for box in boxes:
             pt1 = (box[0], box[1])
             pt2 = (box[2], box[3])
             cv2.rectangle(obs, pt1, pt2, (255,0,0), 2)
+        """
 
-        save_npz(obs, boxes, classes)
+        save_npz(obs, seg,boxes, classes)
+        
+        # MH these counters appear to control how often to output images but how?
         nb_of_steps += 1
         inner_steps += 1
 
         if done or inner_steps % 100 == 0:
-            env.reset()
+            env.reset()  # MH: every 100 inner steps or if simu finished reset the environment
+
+    #MH: stop cycling on mapswhen MAX_STEPS reached
     if nb_of_steps >= MAX_STEPS:
         break
 
--- visual_lane_servoing/solution/src/visual_lane_servoing/src/visual_servoing_activity.py
+++ visual_lane_servoing/solution/src/visual_lane_servoing/src/visual_servoing_activity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[16]:
 
 
 # The function written in this cell will actually be ran on your robot (sim or real). 
@@ -21,11 +21,27 @@ def get_steer_matrix_left_lane_markings(shape):
                                     using the masked left lane markings (numpy.ndarray)
     """
     
-    steer_matrix_left_lane = np.random.rand(*shape)
+    # steer_matrix_left_lane = np.random.rand(*shape)
+    m = shape[0]
+    n = shape[1]
+    
+    C = 0
+    A = (3*n)/4 - C
+    B = (3*n)/(4*m)
+        
+    steer_matrix_left_lane = np.zeros(shape)
 
+    for i in range(0,m):
+        for j in range(0,n):
+            if j > (A - B * i): 
+                steer_matrix_left_lane[i,j] = -1
+    """
+    steer_matrix_left_lane = np.zeros(shape)
+    steer_matrix_left_lane[:,int(2/5*n):n]=-1
+    """
     return steer_matrix_left_lane
 
-# In[ ]:
+# In[17]:
 
 
 # The function written in this cell will actually be ran on your robot (sim or real). 
@@ -42,11 +58,28 @@ def get_steer_matrix_right_lane_markings(shape):
                                      using the masked right lane markings (numpy.ndarray)
     """
     
-    steer_matrix_right_lane = np.random.rand(*shape)
+    # steer_matrix_right_lane = np.random.rand(*shape)
+    m = shape[0]
+    n = shape[1]
+    
+    C = 0
+    A = n/4 + C
+    B = (3*n)/(4*m)
+    
+    steer_matrix_right_lane = np.zeros(shape)
 
+    for i in range(0,m):
+        for j in range(0,n):
+            if j < (A + B*i): 
+                steer_matrix_right_lane[i,j] = 1
+    """
+    steer_matrix_right_lane = np.zeros(shape)
+    steer_matrix_right_lane[:,0:int(3/5*n)]= 1
+    """
+    
     return steer_matrix_right_lane
 
-# In[ ]:
+# In[19]:
 
 
 # The function written in this cell will actually be ran on your robot (sim or real). 
@@ -66,9 +99,62 @@ def detect_lane_markings(image):
             right_masked_img:  Masked image for the solid-white line (numpy.ndarray)
     """
     
-    h, w, _ = image.shape
+    # Most of our operations will be performed on the grayscale version
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Convert the image to HSV for color-based filtering
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    h, w = image_gray.shape
+    # mask_left_edge = np.random.rand(h, w)
+    # mask_right_edge = np.random.rand(h, w)
     
-    mask_left_edge = np.random.rand(h, w)
-    mask_right_edge = np.random.rand(h, w)
+    # 1) mask_ground
+    mask_ground = np.ones((h,w), dtype=np.uint8)
+    horizon = 115 # MH: adjusted manually 170 for real, 115 for simu images
+    mask_ground[0:horizon, :] = 0
+    
+    # 2) mask_left & mask_right
+    fifth_width = int(w/5) # MH: arbitrarily 3/5 of the width either side
+    mask_right = np.ones((h,w), dtype=np.uint8)
+    mask_right[:,0:2*fifth_width] = 0
+    mask_left = np.ones((h,w), dtype=np.uint8)
+    mask_left[:,3*fifth_width:w + 1] = 0
+
+    # 3) Calculate gradients
+    #    3.1) Smooth image with a Gaussian filter
+    SIGMA = 1 # MH: 4 for real images, 1 for simu
+    image_smoothed = cv2.GaussianBlur(image_gray,(0,0), SIGMA)
+
+    #    3.2) Find gradients with Sobel filters
+    sobelx = cv2.Sobel(image_smoothed,cv2.CV_64F,1,0)
+    sobely = cv2.Sobel(image_smoothed,cv2.CV_64F,0,1)
+
+    # 4) mask_sobelx_pos, mask_sobelx_neg & mask_sobely_neg 
+    #    Generate masks that identifies pixels based on the sign of their x-derivative and y-derivative
+    mask_sobelx_pos = np.uint8(sobelx > 0)
+    mask_sobelx_neg = np.uint8(sobelx < 0)
+    mask_sobely_neg = np.uint8(sobely < 0)
+    # mask_sobely_pos = (sobely > 0)
+    
+    # 5) mask_mag
+    Gmag = np.sqrt(sobelx*sobelx + sobely*sobely)
+    THRESHOLD = 40 # MH: 30 for real images, 40 for simu
+    mask_mag = np.uint8(Gmag > THRESHOLD)
+
+    # 6) mask_yellow & mask_white
+    SCALES = [.5,2.55,2.55] # MH: to scale from HSV picker to OpenCV scale
+
+    white_lower_hsv = np.asarray(np.array([0, 0, 35]) * SCALES, dtype=np.int)
+    white_upper_hsv = np.asarray(np.array([260, 20, 105]) * SCALES, dtype=np.int)
+    yellow_lower_hsv = np.asarray(np.array([40, 25, 45]) * SCALES, dtype=np.int) # MH: [40, 45, 45] for real images, [40, 25, 45] for simu
+    yellow_upper_hsv = np.asarray(np.array([60, 105, 90]) * SCALES, dtype=np.int)
+
+    mask_white = np.uint8(cv2.inRange(image_hsv, white_lower_hsv, white_upper_hsv))
+    mask_yellow = np.uint8(cv2.inRange(image_hsv, yellow_lower_hsv, yellow_upper_hsv))
+
+    # Complete set of masks
+    mask_left_edge = mask_ground * mask_left * mask_mag * mask_sobelx_neg * mask_sobely_neg * mask_yellow
+    mask_right_edge = mask_ground * mask_right * mask_mag * mask_sobelx_pos * mask_sobely_neg * mask_white
     
     return (mask_left_edge, mask_right_edge)
